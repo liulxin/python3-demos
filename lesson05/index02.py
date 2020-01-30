b2eefbf02e52db1258d61c68c2b83cd89b1b8c64
liulxin <550293874@qq.com>
2020-01-30 23:03:11 +0800

    lesson05

--- lesson05/index02.py
+++ lesson05/index02.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from enum import IntEnum,unique # 强制 int
 
 class Vip(Enum):
   YELLOW = 1
@@ -6,6 +7,16 @@ class Vip(Enum):
   BLANK = 3
   RED = 4
 
+@unique
+class VipInt(IntEnum):
+  YELLOW = 1
+  # YELLOW_Alias = 1
+  GREEN = 2
+  BLANK = 3
+  RED = 4
+
 res = Vip.YELLOW == 1
 
 print(res) # False
+
+print(Vip(1)) # Vip.YELLOW
---
+++ lesson05/index03.py
@@ -0,0 +1,15 @@
+def curve_pre():
+  a = 25
+  def curve(x):
+    return a * x * x
+  return curve
+
+a = 10
+
+f = curve_pre()
+
+print(f(2)) # 100
+
+print(f.__closure__)
+
+print(f.__closure__[0].cell_contents) # 25
