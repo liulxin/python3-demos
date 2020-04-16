7aa951a664a5c4ecb2d95f18c79e121b7701b46b
liulxin <550293874@qq.com>
2020-04-17 00:27:48 +0800

    day01

---
+++ boy/day01/t1.py
@@ -0,0 +1,26 @@
+name = 'dong'
+SEX = 'MAN'
+
+print('hello world')
+
+a = 1
+print(a, type(a)) # <class 'int'>
+
+s1 = 'day01'
+s2 = 'day02'
+s3 = '''
+      可以换行
+    '''
+
+print(s1 , type(s1)) # <class 'str'>
+print(s2)
+print(s3)
+print(s1 + s2) # day01day02
+print(s2 * 2) # day02day02
+
+print(2 > a, type(2 > a)) # True <class 'bool'>
+
+username = input('please enter your name? ')
+password = input('please enter your password? ')
+print(username)
+print(password)
