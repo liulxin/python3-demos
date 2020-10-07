b05078f336fd1d6044cb9eaffd45826bd4a1479f
董元节 <550293874@qq.com>
2020-10-07 15:26:49 +0800

    py

---
+++ sixhour/1004_2.py
@@ -0,0 +1,9 @@
+name = 'dong wudi'
+
+print(name[1:-1])
+
+name2 = f'{name}two'
+
+print(name2)
+
+print(name.title())
---
+++ sixhour/1004_4.py
@@ -0,0 +1,12 @@
+is_hot = True
+is_cold = False
+
+if is_hot:
+  print("it's a hot day")
+  print('drink plenty of water')
+elif is_cold:
+  print("it's a cold day")
+  print('wear warm clothes')
+else:
+  print('enjoy your day')
+  
